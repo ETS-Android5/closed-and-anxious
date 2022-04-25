5b42228df54ccd5d91c7ec2485a981996604f54b
ETALATE <84042296+ETALATE@users.noreply.github.com>
2022-04-25 20:29:27 +0200

    TrackUI setup with MPV functionality - works with one track

--- app/src/main/java/dk/itu/closed_and_anxious/MainActivity.java
+++ app/src/main/java/dk/itu/closed_and_anxious/MainActivity.java
@@ -1,79 +1,33 @@
 package dk.itu.closed_and_anxious;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.track_ui);
+        setContentView(R.layout.activity_main);
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment trackUI = fm.findFragmentById(R.id.container_ui);
+        trackUI = new TrackUI();
+
+        fm.beginTransaction().add(R.id.container_ui, trackUI).commit();
     }
-
-    public void play(View v) {
-        System.out.println("~~~~~~~~~~~~ STARTING: Start playing Track");
-        // if player is not created
-        if (player == null) {
-            // create a player with given track
-            player = MediaPlayer.create(v.getContext(), R.raw.hurrystress);
-            // when the player has completed a track
-            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mediaPlayer) {
-                    // we call the stopPlayer to release mp resources
-                    stopPlayer();
-
-                }
-            });
-
-        }
-
-        // start the player (no matter if just created or already exists)
-        player.start();
-        System.out.println("~~~~~~~~~~~~ SUCCESS: Started playing Track");
-
-
-    }
-
-    public void pause(View v) {
-        System.out.println("~~~~~~~~~~~~ STARTING: Pausing Track");
-        // if player exists
-        if (player != null) {
-            // pause the track of the player
-            player.pause();
-            System.out.println("~~~~~~~~~~~~ SUCCESS: Paused Track");
-        }
-
-    }
-
-    public void stop(View v) {
-        stopPlayer();
-        System.out.println("~~~~~~~~~~~~ Starting: Stopping Track");
-
-        System.out.println("~~~~~~~~~~~~ SUCCESS: Stopped Track");
-    }
-
-    /*
-        Called when the media player is stopped and when the media player has finished a song.
-     */
-    public void stopPlayer() {
-        // if player is created
-        if (player != null) {
-            // release its resources
-            player.release();
-            // set the player to null (so a new one can be created)
-            player = null;
-            // Toast stating that the player has stopped
-            //Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
-        }
-    }
-
 
 }
+
+
+
