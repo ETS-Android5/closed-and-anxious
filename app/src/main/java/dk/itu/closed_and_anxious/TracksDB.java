fc9897a88383f144f18b18cd2590085b4bc82b84
ETALATE <84042296+ETALATE@users.noreply.github.com>
2022-04-28 13:17:44 +0200

    comments in TrackDB

--- app/src/main/java/dk/itu/closed_and_anxious/TracksDB.java
+++ app/src/main/java/dk/itu/closed_and_anxious/TracksDB.java
@@ -16,26 +16,50 @@ import dk.itu.closed_and_anxious.database.TrackDBSchema;
 public class TracksDB extends ViewModel { // ViewModel - instanciated in activity it is kept and will live as long as the activity does
     private static SQLiteDatabase trackDatab;
 
+    /**
+     * Method for initializing the database.
+     *
+     * @param context
+     */
     public void initialize(Context context){
         if (trackDatab == null) {
             trackDatab = new TrackBaseHelper(context.getApplicationContext()).getWritableDatabase();
-            if (getValues().size() == 0) fillItemsDB();
+            if (getValues().size() == 0)  {
+                fillItemsDB();
+            }
         }
     }
 
+    /**
+     * Method for adding tracks to the database.
+     *
+     * @param k the key of a track as resource id
+     * @param n the name of a track
+     * @param d the description of a track
+     * @param c the category of a track
+     * @param i the image of a track as a resource id
+     */
     public void addTrack(int k, String n, String d, String c, int i){
         Track newTrack = new Track(k, n, d, c, i);
         ContentValues values = getContentValues(newTrack);
         trackDatab.insert(TrackDBSchema.TrackTable.TABLENAME, null, values);
     }
 
-
+    /**
+     * Method for populating the database.
+     * Invoked in the initialize method, if the database is not already populated.
+     */
     public void fillItemsDB() {
         addTrack(1800000, "Gently Does It", "An atmospheric, uplifting soundscape. Credit: Ketsa", "Noise", 700112);
         System.out.println("PRINTED");
     }
 
-
+    /**
+     * Helper method for getting all track objects (rows) from the database.
+     * Invoked in initialize method, to check if the database is populated.
+     *
+     * @return an ArrayList containing all the track objects from the database.
+     */
     public ArrayList<Track> getValues() {
         ArrayList<Track> tracks = new ArrayList<Track>();
         TrackCursorWrapper cursor = queryItems(null, null);
@@ -49,6 +73,15 @@ public class TracksDB extends ViewModel { // ViewModel - instanciated in activit
         return tracks;
     }
 
+    /**
+     * Helper method for getValues to send a query to the database.
+     * The query requests all the information (columns) for one track in the database.
+     *
+     * @param whereClause is set to null.
+     * @param whereArgs is set to null.
+     * @return a new TrackCursorWrapper object based on the result set from the query contained by the cursor.
+     */
+
     static private TrackCursorWrapper queryItems(String whereClause, String[] whereArgs) {
         Cursor cursor = trackDatab.query(
                 TrackDBSchema.TrackTable.TABLENAME,
@@ -61,7 +94,13 @@ public class TracksDB extends ViewModel { // ViewModel - instanciated in activit
         return new TrackCursorWrapper(cursor);
     }
 
-    // Database helper methods to convert between Items and database rows
+    /**
+     * Database helper Method for setting up a track object to be stored in the database as a row.
+     * Used in addTrack method, a ContentValues object stores the track values for later insertion in the database.
+     *
+     * @param track
+     * @return a ContentValues object with the values of the track.
+     */
     private static ContentValues getContentValues(Track track) {
         ContentValues values =  new ContentValues();
         values.put(TrackDBSchema.TrackTable.Columns.TRACKEY, track.getKey());
